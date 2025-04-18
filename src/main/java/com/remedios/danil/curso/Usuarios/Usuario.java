810a735dfdfe9b870e99f2f65f1c5de471ff3293
Danilomr305 <danilomr305@gmail.com>
2025-04-18 12:50:04 -0300

    Metodo para security de usuario em criação

---
+++ src/main/java/com/remedios/danil/curso/Usuarios/Usuario.java
@@ -0,0 +1,26 @@
+package com.remedios.danil.curso.Usuarios;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Table(name = "Usuario")
+@Entity(name = "usuarios")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String login;
+    private String senha;
+}
